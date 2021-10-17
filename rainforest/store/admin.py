6b28b1f7bdeb2cadb67c21dbf3f502e188fc99a0
Kirill <mashenkovkirill@gmail.com>
2021-10-17 17:37:39 +0300

    created projec, docker compose and models

---
+++ rainforest/store/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Order, Product, ProductsInOrder
+
+admin.site.register(Order)
+admin.site.register(Product)
+admin.site.register(ProductsInOrder)
+
+# Register your models here.
